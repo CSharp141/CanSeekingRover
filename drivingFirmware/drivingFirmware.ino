652bd194fe0ca43ab5fdb5937c42d41b30aab3af
rowan-gourlay <65909348+rowan-gourlay@users.noreply.github.com>
2025-05-02 13:45:23 +0100

    drivingFirmware.ino with crane code

--- drivingFirmware/drivingFirmware.ino
+++ drivingFirmware/drivingFirmware.ino
@@ -53,6 +53,32 @@ inline int pctToPulse(int pct, int mn, int mx) {
   return mn + (mx - mn) * pct / 100;
 }
 
+// Motor Pins
+
+const int YIN1 = 5; //pins for up/down movement
+const int YIN2 = 22;
+const int YIN3 = 21;
+const int YIN4 = 23;
+
+const int XIN1 = 12; //pins for horizontal movement
+const int XIN2 = 14;
+const int XIN3 = 32;
+const int XIN4 = 33;
+
+const int STEP_DELAY_MS = 1; //step delay for motors
+
+// Half-step sequence (8 steps)
+const int step_sequence[8][4] = {
+    {1, 0, 0, 0}, // A
+    {1, 1, 0, 0}, // AB
+    {0, 1, 0, 0}, // B
+    {0, 1, 1, 0}, // BC
+    {0, 0, 1, 0}, // C
+    {0, 0, 1, 1}, // CD
+    {0, 0, 0, 1}, // D
+    {1, 0, 0, 1}  // DA
+};
+
 // —— Motor & Steering Tasks (50 Hz) ——  
 void motorTask(void*) {
   Servo esc;
@@ -113,6 +139,15 @@ void controlTask(void*) {
       case Mode::Pickup:
         throttlePct = 50;
         steerPct    = 50;
+        //lower crane fully
+        step_motor(512, -1, true);}
+
+        //sweep crane 
+        for (i=0;i<2;++i){ 
+          step_motor(512, -1, false);}
+        for (i=0;i<4;++i){ 
+          step_motor(512, 1, false);}
+          
         break;
 
       case Mode::Return:
@@ -219,6 +254,28 @@ void handleMode() {
   server.send(200, "text/plain", "OK");
 }
 
+//Set motor pins to step sequence
+void set_step(const int step[4], bool motor) {
+  if (motor) { // TRUE for UP/DOWN
+    digitalWrite(YIN1, step[0]);
+    digitalWrite(YIN2, step[1]);
+    digitalWrite(YIN3, step[2]);
+    digitalWrite(YIN4, step[3]);
+  } else { // FALSE for LEFT/RIGHT
+    digitalWrite(XIN1, step[0]);
+    digitalWrite(XIN2, step[1]);
+    digitalWrite(XIN3, step[2]);
+    digitalWrite(XIN4, step[3]);
+  }
+}
+void step_motor(int steps, int direction, bool motor) {
+  for (int i = 0; i < steps; i++) {
+    int idx = (direction > 0) ? (i % 8) : (7 - (i % 8));
+    set_step(step_sequence[idx], motor);
+    delay(STEP_DELAY_MS);
+  }
+}
+
 void setup() {
   // UART0 for seeker input (and optional debug)
   Serial.begin(115200);
@@ -238,6 +295,22 @@ void setup() {
     Serial.println("ESC calibration done.");
   }
 
+  // UP/DOWN pins
+  pinMode(YIN1, OUTPUT);
+  pinMode(YIN2, OUTPUT);
+  pinMode(YIN3, OUTPUT);
+  pinMode(YIN4, OUTPUT);
+
+  // LEFT/RIGHT pins
+  pinMode(XIN1, OUTPUT);
+  pinMode(XIN2, OUTPUT);
+  pinMode(XIN3, OUTPUT);
+  pinMode(XIN4, OUTPUT);
+
+  //sets for can height
+  for (i=0;i<5;++i){ //change for height
+          step_motor(512, -1, true);}
+
   server.on("/",     handleRoot);
   server.on("/mode", handleMode);
   server.begin();
@@ -250,4 +323,3 @@ void setup() {
 void loop() {
   server.handleClient();
 }
-
